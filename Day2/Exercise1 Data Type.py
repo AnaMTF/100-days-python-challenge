041ca6f1e94c85701de780d09b1f9107a156835d
Anamaria <74465055+AnaMTF@users.noreply.github.com>
2023-06-08 21:13:42 +0300

    Update and rename Ex1 to Exercise1 Data Type.py

---
+++ Day2/Exercise1 Data Type.py
@@ -0,0 +1,34 @@
+#Data Types
+
+#String
+
+print("Hello"[3])
+
+print("123" + "345")
+
+#Integer
+
+print(123+345)
+
+123_345_456_953
+
+#Float
+
+3.14159
+
+#Bool
+
+True
+False
+
+#num_char = len(input("what is ur name?"))
+
+# print(type(num_char))
+
+# str_num_char = str(num_char)
+
+# print("Your name has " + str_num_char + " characters")
+
+print(70 + float("100.5"))
+
+print(str(70)+str(100.5))
