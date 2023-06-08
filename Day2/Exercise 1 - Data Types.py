08c51249be33ff65f14f85da8c0ac7af34f0705d
Anamaria <74465055+AnaMTF@users.noreply.github.com>
2023-06-08 21:18:18 +0300

    Create Exercise 1 - Data Types.py

---
+++ Day2/Exercise 1 - Data Types.py
@@ -0,0 +1,9 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+number1 = int(two_digit_number[0])
+number2 = int(two_digit_number[1])
+print (number1 + number2)
