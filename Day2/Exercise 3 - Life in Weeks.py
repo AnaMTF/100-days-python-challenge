9f46a2bb94477bbf3b301d4fb4111461558570c5
Anamaria <74465055+AnaMTF@users.noreply.github.com>
2023-06-08 21:47:07 +0300

    Create Exercise 3 - Life in Weeks.py

---
+++ Day2/Exercise 3 - Life in Weeks.py
@@ -0,0 +1,13 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+months = (90 - int(age)) * 12
+
+weeks = (90 - int(age)) * 52
+
+days = (90 - int(age)) * 365
+
+print (f"You have {days} days, {weeks} weeks, and {months} months left.")
