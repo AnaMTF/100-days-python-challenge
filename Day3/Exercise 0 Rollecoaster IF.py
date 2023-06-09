7a2564889794a5cbf47dc23608325c2fbd5b450a
Anamaria <74465055+AnaMTF@users.noreply.github.com>
2023-06-09 23:27:02 +0300

    Update Exercise 0 Rollecoaster IF.py

--- Day3/Exercise 0 Rollecoaster IF.py
+++ Day3/Exercise 0 Rollecoaster IF.py
@@ -3,9 +3,9 @@ height = int(input("What is your height in cm? "))
 age = int(input("What is your age? "))
 if height >= 120:
   print("You can ride the rollercoaster.")
-  if age < 12:
+  if age <= 12:
     print("You have to pay $5.")
-  elif age > 18:
+  elif age >= 18:
     print("You have to pay $12.")
   else:
     print("You have to pay $7.")
