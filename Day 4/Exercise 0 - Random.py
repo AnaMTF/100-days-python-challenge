e4028c2fe727eb9778ee5ed994cb99cdf94f47d1
Anamaria <74465055+AnaMTF@users.noreply.github.com>
2023-06-10 22:19:50 +0300

    Create Exercise 0 - Random.py

---
+++ Day 4/Exercise 0 - Random.py
@@ -0,0 +1,13 @@
+import random
+
+random_integer = random.randint(1, 10)
+
+print(random_integer)
+
+random_float = 5 * random.random()
+
+print(random_float)
+
+love_score = random.randint(1,100)
+
+print(f"Your love score is {love_score}")
