a9f61b1bcea2b8bc12f0ff4858bee62e1282c06d
Anamaria <74465055+AnaMTF@users.noreply.github.com>
2023-06-10 22:42:08 +0300

    Update Exercise 0 - Random.py

--- Day 4/Exercise 0 - Random.py
+++ Day 4/Exercise 0 - Random.py
@@ -8,6 +8,16 @@ random_float = 5 * random.random()
 
 print(random_float)
 
-love_score = random.randint(1,100)
+love_score = random.randint(1, 100)
 
 print(f"Your love score is {love_score}")
+
+states_of_america = [
+    "California",  "Mississippi", "Delware",
+    "Pennsylvania"
+]
+
+states_of_america.append("Arkansas")
+
+print(states_of_america[-1])
+
