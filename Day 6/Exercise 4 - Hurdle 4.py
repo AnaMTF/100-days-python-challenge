ec872b49b5226f1e713a3ab61dc20dad3a8ef4fa
Anamaria <74465055+AnaMTF@users.noreply.github.com>
2023-06-12 21:47:39 +0300

    Create Exercise 4 - Hurdle 4.py

---
+++ Day 6/Exercise 4 - Hurdle 4.py
@@ -0,0 +1,33 @@
+def turn_around():
+    turn_left()
+    turn_left()
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def square():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+def jump():
+    turn_left()
+    while wall_on_right():
+        move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    while wall_on_right() and front_is_clear():
+        move()
+    turn_left()
+while not at_goal():
+    if front_is_clear():
+        move()
+    else:
+        jump()
