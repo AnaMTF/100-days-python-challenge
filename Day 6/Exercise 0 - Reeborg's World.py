3dbe12c599f1a432be61c3779d21d71124bfce3d
Anamaria <74465055+AnaMTF@users.noreply.github.com>
2023-06-12 21:28:05 +0300

    Update Exercise 0 - Reeborg's World.py

--- Day 6/Exercise 0 - Reeborg's World.py
+++ Day 6/Exercise 0 - Reeborg's World.py
@@ -26,17 +26,9 @@ def jump():
     turn_left()
 #for i in range(6):
 #    jump()
-#for the hurdle 1  
+ 
 nr_herdles = 6
 while nr_herdles > 0:
     jump()
     nr_herdles -=1
-#hurdle 2
-while not at_goal():
-    jump()
-#hurdle 3
-while not at_goal():
-    if front_is_clear():
-        move()
-    else:
-        jump() #note that i erased the first move() from jump() so it doesnt crash in a wall
+
