f980c851025ba09095c18e93fcd2aba1efe49573
Anamaria <74465055+AnaMTF@users.noreply.github.com>
2023-06-12 21:32:08 +0300

    Create Exercise 2 - Hurdle 2.py

---
+++ Day 6/Exercise 2 - Hurdle 2.py
@@ -0,0 +1,28 @@
+def turn_around():
+    turn_left()
+    turn_left()
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+
+def square():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+def jump():
+    move()
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+while not at_goal():
+    jump()
