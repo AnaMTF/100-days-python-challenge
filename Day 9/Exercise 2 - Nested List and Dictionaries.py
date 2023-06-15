1c247a1013f25562d0235bac59f920228770a440
AnaMTF <anamaria.titeche@gmail.com>
2023-06-15 18:25:07 +0300

    nested lists and dictionaries

--- Day 9/Exercise 2 - Nested List and Dictionaries.py
+++ Day 9/Exercise 2 - Nested List and Dictionaries.py
@@ -7,8 +7,22 @@ capitals = {
 #Nesting a List in a Dictionary
 
 travel_log = {
-    "France" : {"cities_visited": ["Paris", "Dijon", "Lille"], "total_visits": 12},
-    "Germany" : {"cities_visited":["Berlin", "Hamburg", "Stuttgart"], "total_visits": 5},
+    "France" : {"cities_visited": ["Paris", "Dijon", "Lille"],
+                "total_visits": 12},
+    "Germany" : {"cities_visited":["Berlin", "Hamburg", "Stuttgart"],
+                 "total_visits": 5},
 }
 
 print(travel_log)
+
+#Nesting a Dictionary in a List
+travel_log = [
+    {
+        "country": "France",
+        "cities_visited": ["Paris", "Dijon", "Lille"],
+        "total_visits": 12},
+    {
+        "country": "Germany",
+        "cities_visited":["Berlin", "Hamburg", "Stuttgart"],
+        "total_visits": 5},
+]
